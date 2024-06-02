5daf41fbafbb37332a74100ef871808ab25f43fb
Custodian <thehalfstackdev@gmail.com>
2024-06-02 18:40:00 +0200

    feat: setup jest and react testing boilerplate

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  /**
+   * Provide the path to your Next.js app to load
+   * next.config.js and .env files in your test environment
+   */
+  dir: "./",
+});
+
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^@/lib/(.*)$": "<rootDir>/lib/$1",
+    "^@/styles/(.*)$": "<rootDir>/styles/$1",
+  },
+};
+
+export default createJestConfig(config);
